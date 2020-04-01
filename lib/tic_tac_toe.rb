3347101ccae383b5a036a1c4e792f662d324dcaf
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-01 00:07:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,8 @@
 class TicTacToe
 
+  # Initialize method; every time this is called,
+  # the instance variable board is set to the game's
+  # initial state. 
   def initialize
     @board = []
   end
